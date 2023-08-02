f97baef89e26e231495b5f4ac3413c6ae11afe38
nfty <eatmeimadanish@gmail.com>
2023-08-02 07:29:17 -0500

    velox updates

--- API/scheduling/everyday.py
+++ API/scheduling/everyday.py
@@ -2,9 +2,8 @@ from gevent import spawn
 import schedule
 
 def midnight():
-    pass
-#     from API.PMS import velox
-#     spawn(velox.reset)
+    from API.PMS import velox
+    velox.reset()
 
 def set():
     schedule.every().day.at("00:00").do(midnight)
