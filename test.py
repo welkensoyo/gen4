09de7da896edaedc9af37808d414d5cb1f7f576b
nfty <eatmeimadanish@gmail.com>
2023-10-04 09:39:12 -0500

    velox updates

--- test.py
+++ test.py
@@ -1,13 +1,2 @@
-# from API.PMS import velox
-# velox.reset_table('ledger')
-# velox.reset_table('treatments')
-#
-import traceback
-x ={}
-
-try:
-    y = x['test']
-except:
-    e = traceback.format_exc()
-
-print(e)
+from API.PMS import velox
+velox.reset_table('treatments')
