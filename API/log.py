3b1666b5ce9c9ff6b6c2007854a9b5a8e992114e
nfty <eatmeimadanish@gmail.com>
2024-06-07 09:10:08 -0500

    velox updates

--- API/log.py
+++ API/log.py
@@ -5,20 +5,22 @@ import arrow
 
 def log(route, method, payload, code, result):
     def _(route, method, payload, code, result):
-        PSQL = ''' INSERT INTO dev.api_log (route, method, payload, response_code, response_message) VALUES (%s, %s, %s, %s, %s) '''
         try:
+            PSQL = ''' INSERT INTO dev.api_log (route, method, payload, response_code, response_message) VALUES (?, ?, ?, ?, ?) '''
             dbpy.execute(PSQL, route, method, payload, code, result)
         except:
+            PSQL = ''' INSERT INTO dev.api_log (route, method, payload, response_code, response_message) VALUES (%s, %s, %s, %s, %s) '''
             db.execute(PSQL, route, method, payload, code, result)
     spawn(_, route, method, payload, code, result)
 
 
 def velox_log(mode=None, error=''):
     if mode:
-        SQL = "UPDATE dbo.vx_log SET last_sync=GETDATE() AT TIME ZONE 'Central Standard Time', error=? WHERE [mode] = ?"
         try:
+            SQL = "UPDATE dbo.vx_log SET last_sync=GETDATE() AT TIME ZONE 'Central Standard Time', error=? WHERE [mode] = ?"
             return dbpy.execute(SQL, error, mode)
         except:
+            SQL = "UPDATE dbo.vx_log SET last_sync=GETDATE() AT TIME ZONE 'Central Standard Time', error=%s WHERE [mode] = %s"
             return db.execute(SQL, error, mode)
     SQL = "SELECT mode, CONVERT(VARCHAR, last_sync, 120), error FROM dbo.vx_log"
     try:
