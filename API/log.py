c5c038fcc934575be7ce493ecabd43f02ce61e86
nfty <eatmeimadanish@gmail.com>
2024-09-27 22:08:43 -0500

    Add argparse for log redirection and streamline main execution
    Introduced argparse to manage log redirection in start.py, simplifying command-line argument parsing for more flexible logging. Additionally, updated test.py to include a reset_table function call on 'providers' to ensure proper table resets during testing.

--- API/log.py
+++ API/log.py
@@ -22,10 +22,10 @@ def sync_log(table, pids, result):
 def velox_log(mode=None, error=''):
     try:
         if mode:
-            SQL = "UPDATE dbo.vx_log SET last_sync=GETDATE() AT TIME ZONE 'Central Standard Time', error=%s WHERE [mode] = %s"
-            dbms.execute(SQL, error, mode)
+            SQL = "UPDATE dbo.vx_log SET last_sync=GETDATE() AT TIME ZONE 'Central Standard Time', error=? WHERE [mode] = ?"
+            dbpy.execute(SQL, error, mode)
         SQL = "SELECT mode, CONVERT(VARCHAR, last_sync, 120), error FROM dbo.vx_log"
-        return [(x[0], arrow.get(x[1]).to('US/Central').format('YYYY-MM-DD HH:mm:ss'), x[2]) for x in dbms.fetchall(SQL)]
+        return [(x[0], arrow.get(x[1]).to('US/Central').format('YYYY-MM-DD HH:mm:ss'), x[2]) for x in dbpy.fetchall(SQL)]
     except:
         return []
 
@@ -37,8 +37,8 @@ def velox_sync(status=None):
     if status not in (None, 'running', 'idle'):
         return False
     if status:
-        SQL = ''' UPDATE dev.settings SET value = %s WHERE setting = 'sync_state' '''
-        dbms.execute(SQL, status)
+        SQL = ''' UPDATE dev.settings SET value = ? WHERE setting = 'sync_state' '''
+        dbpy.execute(SQL, status)
         return status
     SQL = ''' SELECT value FROM dev.settings WHERE setting = 'sync_state' '''
     status = dbms.fetchall(SQL)[0][0]
